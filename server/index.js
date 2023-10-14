9c50b3267ee826101ed02a3f61ae0ca6878beb26
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2023-10-14 23:49:29 +0530

    boilerplate server setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const app = express();
-
 
 app.get('/', (req, res) => {
     res.send("Welcome to Home page ")
 })
 
-
-
-app.listen(3000, () => {
-    console.log('Running on port 3000');
+app.listen(process.env.PORT, async () => {
+    mongoose.connect(process.env.MONGODB_URL)
+    console.log(`server running on port ${process.env.PORT}`);
 })
