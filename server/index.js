2797dbf10f33e54585e97aeddabc60441e6354fa
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2023-10-23 23:21:09 +0530

    added database config and initialzation

--- server/index.js
+++ server/index.js
@@ -1,16 +1,93 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
 
 dotenv.config();
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(express.static('./public'))
+app.use(cors());
+
+const User = mongoose.model('User', {
+    Name: String,
+    Email: String,
+    Mobile: String,
+    Password: String,
+});
 
 app.get('/', (req, res) => {
-    res.send("Welcome to Home page ")
+    res.send("Home Page")
 })
 
-app.listen(process.env.PORT, async () => {
-    mongoose.connect(process.env.MONGODB_URL)
-    console.log(`server running on port ${process.env.PORT}`);
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server is up and running" })
 })
+
+//GET /users (Read)
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json({
+            status: 'SUCCESS',
+            data: users
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: 'FAIL',
+            message: 'Something went wrong'
+        })
+    }
+});
+
+// POST /users (Create)
+app.post('/users', async (req, res) => {
+    try {
+        const { Name, Email, Mobile, Password } = req.query;
+        await User.create({ Name, Email, Mobile, Password })
+        res.json({
+            status: 'SUCCESS',
+            message: 'User added successfully!'
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: 'FAIL',
+            message: 'Something went wrong'
+        })
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    mongoose
+        .connect(process.env.MONGODB_URL)
+        .then(() => console.log(`Server running on ${process.env.PORT}`))
+        .catch(error => console.log(error));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
